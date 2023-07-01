1c92aea98776c25942f374282245263e11888ce6
ja-cha <jan_abt@yahoo.com>
2023-07-01 17:52:24 +0700

    adding main, outputs & variables

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  key_name      = var.key_name
+  tags = {
+    Name = var.name
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = var.name
+  ingress {
+    from_port   = var.ssh_port
+    to_port     = var.ssh_port
+    protocol    = "tcp"
+    cidr_blocks = var.allow_ssh_from_cidrs
+  }
+}
+
+
